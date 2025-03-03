4a3b2643327db2e398781a2fd05adff10ed9a64d
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-03-03 19:41:26 +0900

    refactor: SlideListArea

---
+++ src/app/SlideList.tsx
@@ -0,0 +1,5 @@
+export const SlideList = [
+  { src: "/first-pic.JPG", alt: "Profile image" },
+  { src: "/first-pic2.png", alt: "Profile image" },
+  { src: "/first-pic3.png", alt: "Profile image" },
+];
---
+++ src/app/SlideListArea.tsx
@@ -0,0 +1,66 @@
+"use client";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { SlideList } from "./SlideList";
+import Image from "next/image";
+
+export default function SlideListArea() {
+  return (
+    <div className="relative">
+      <h1
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold z-50 animate-fadeInSlide"
+        style={{
+          fontFamily: "Cherry Bomb One",
+          fontWeight: 400,
+          fontStyle: "normal",
+          color: "#FBCFE8",
+          textShadow: "4px 4px 4px rgba(0, 0, 0, 0.2)",
+          fontSize: "100px",
+        }}
+      >
+        saki's portfolio
+      </h1>
+      <Swiper
+        className="w-full h-screen"
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        loop
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+      >
+        {SlideList.map((picture, index) => (
+          <SwiperSlide key={index}>
+            <div className="w-full h-screen relative">
+              <Image
+                src={picture.src}
+                alt={picture.alt}
+                fill
+                style={{ objectFit: "cover" }}
+                priority
+              />
+              <div className="absolute top-0 left-0 w-full h-full bg-black/60" />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
