d79a680e22e0adc619b5a550f414bd08550e7f05
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-03-03 17:46:48 +0900

    chore: 環境構築

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
+
+export default config;
